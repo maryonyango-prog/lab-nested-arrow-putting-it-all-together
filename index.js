b1de98131006f650d8669b0f827456729a8a8532
Onyango Mary <adongorowa@gmail.com>
2025-12-07 11:15:04 +0300

    Login tracker function with closure

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
+// Simple 3-attempt login with named functions
+//Aquiring userInfo
+function UserInfo(userName, password) {
+    return {
+        userName: userName,
+        password: password
+    };
 
+}
 
+function createLoginTracker(userInfo) {
+    let attemptCount = 0;
+    
+return (passwordAttempt) => {
+    attemptCount++;
 
+//too many attempts
+    if (attemptCount > 3) {
+        return "Account locked due to too many failed login attempts";
+    }
+ //Successfull login   
+    if (passwordAttempt === userInfo.password) {
+        return "Login successful";
+    }
+return `Attempt ${attemptCount}: Login failed`;
+
+};
+}
+//Export
 module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+    ...(typeof createLoginTracker!== 'undefined' && { createLoginTracker })
 };
